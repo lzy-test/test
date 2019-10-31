e787b1780e867bdcc0b46d0d807ed2dbeeb17da7
lzy123456 <183180767@qq.com>
2019-10-31 16:19:27 +0800

    代码跟新

--- test_case/models/function.py
+++ test_case/models/function.py
@@ -8,14 +8,14 @@ class Function:
     def insert_img(driver, file_name):
         base_dir = os.path.dirname(os.path.dirname(__file__))
         base_dir = str(base_dir)
-        # print(base_dir)
+        print(base_dir)
         base_dir = base_dir.replace('\\', '/')
-        # print(base_dir)
+        print(base_dir)
         base = base_dir.split('test_case')[0]
-        # print(base)
         now = time.strftime("%m-%d %H-%M")
+        print(base)
         file_path = base + "/report/image/" + now + "_" + file_name
-        # print(file_path)
+        print(file_path)
         driver.get_screenshot_as_file(file_path)
 
 #测试截图
