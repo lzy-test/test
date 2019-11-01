cb6e72cd160b75daf1422154967a6a5308ea2b2e
lzy123456 <183180767@qq.com>
2019-11-01 17:07:35 +0800

    代码跟新

--- test_case/models/driver.py
+++ test_case/models/driver.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 # 启动浏览器驱动
 def browser():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(r"C:\Users\Administrator.W8-201610091957\AppData\Local\Google\Chrome\Application\chromedriver.exe")
     return driver
 
 if  __name__ == '__main__':
